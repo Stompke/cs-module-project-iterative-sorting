4911970272b618714e3703d872c876f59fb801d5
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-05-18 13:39:23 -0700

    buble sort passing tests

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -10,25 +10,32 @@ def selection_sort(arr):
         for j in range(cur_index + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
-        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     
-    return arr
-
-        
-
-
         # TO-DO: swap
         # Your code here
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
-
+    # current_1 = 0
+    # current_2 = 1
+    number_of_sorted = 0
+    if len(arr) != 0:
+        while number_of_sorted != len(arr) - 1:
+            number_of_sorted = 0
+            for i in range(0, len(arr) - 1):
+                if arr[i + 1] < arr[i]:
+                    arr[i + 1], arr[i] = arr[i], arr[i + 1]
+                else: 
+                    number_of_sorted += 1
     return arr
+
+my_arr = [3,2,1,7,4,33,11,62,23,21,11,56,15,34]
+# my_arr = [3]
+print(bubble_sort(my_arr))
 
 
 # STRETCH: implement the Count Sort function below
